5c73d61bfb5742c9fa0733487d37e53fe493398d
Mahmoud Hussien <mahmoud.abdelmaqsoud.hussien@gmail.com>
2025-09-25 00:30:26 +0300

    Solve Max Words Can Type problem

---
+++ maxWordsCanType.cpp
@@ -0,0 +1,58 @@
+
+
+#include <iostream>
+#include <vector>
+#include <sstream>
+using namespace std;
+
+int maxWordsCanType(string str, char brokenLetter) 
+{
+    vector<string> words;
+    string current = "";
+    
+    for (int i = 0; i < str.length(); i++) 
+    {
+        if (str[i] == ' ') 
+        { 
+            if (!current.empty()) 
+            {
+                words.push_back(current);
+                current = "";
+            }
+        } 
+        else 
+        {
+            current += str[i];
+        }
+    }
+
+    if (!current.empty()) 
+        words.push_back(current);
+    
+
+    int count = 0;
+    for (const string& word : words) 
+    {
+        bool hasBrokenLetter = false;
+        
+        for (char ch : word) 
+        {
+            if (ch == brokenLetter) 
+            {
+                hasBrokenLetter = true;
+                break;
+            }
+        }
+        
+        if (!hasBrokenLetter) 
+            count++; 
+    }
+
+    return count;
+}
+
+int main()
+{
+    cout << maxWordsCanType("hello world", 'd') << "\n";
+    return 0;
+}
